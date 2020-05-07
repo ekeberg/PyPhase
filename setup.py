b4be5a6215a838938b899cd2ddc8b6d6d2ebc62f
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2020-05-07 11:45:06 +0200

    Fairly stable with basic functionality

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name="PyPhase",
+      version="0.1",
+      description="Phase retrieval tools",
+      author="Tomas Ekeberg",
+      packages=["PyPhase"]
+      )
