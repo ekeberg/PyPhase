276e5ffba988e8acb0780c238a9b5f4e373bb258
Tomas Ekeberg <tomas.ekeberg@gmail.com>
2023-01-20 14:05:11 +0100

    Added type-hints Restructured file slightly Added option to copy algorithm (same as link but not connected) Added function to reset algorithm with random phases Added option to iterate() to do multiple iterations Added function to do ER refinement followed by support update

--- PyPhase/pyphase.py
+++ PyPhase/pyphase.py
@@ -1,5 +1,11 @@
 """A collection of tools for single-particle phase retrieval"""
+from __future__ import annotations
+
+import collections
 import enum
+import typing
+from collections.abc import Iterable
+
 
 try:
     import cupy
@@ -13,7 +19,7 @@ class Backend(enum.Enum):
     CUPY = 2
 
 
-def set_backend(new_backend):
+def set_backend(new_backend: Backend) -> None:
     """Select if calculations are run on CPU or GPU"""
     global backend, numpy, scipy, fft
     global ndimage, real_type, complex_type
@@ -37,12 +43,12 @@ def set_backend(new_backend):
         raise ValueError(f"Unknown backend {backend}")
 
 
-def cupy_on():
+def cupy_on() -> bool:
     """Check if the backend is Backend.CUPY"""
     return backend == Backend.CUPY
 
 
-def cpu_on():
+def cpu_on() -> bool:
     """Check if the backend is Backend.CPU"""
     return backend == Backend.CPU
 
@@ -71,7 +77,7 @@ class ChangingVariable:
     def __init__(self, *vars):
         self.points = sorted(vars, key=lambda e: e[0])
 
-    def __call__(self, iteration):
+    def __call__(self, iteration: int) -> float:
         if iteration < self.points[0][0]:
             return self.points[0][1]
         if iteration > self.points[-1][0]:
@@ -88,100 +94,18 @@ class ChangingVariable:
         return value
 
 
-class ModifyAlgorithm:
-    """Abstract class for algorithms that work on phasing algorithms"""
-    def __init__(self):
-        pass
-
-    def add_variable(self, variable, name):
-        if not callable(variable):
-            # Hack to get the current 'variable' back, otherwise it
-            # will capture the overwritten function itself
-            def variable(iteration, v=variable):
-                return v
-            # variable = lambda iteration, v=variable: v
-        self.__dict__[name] = variable
-
-    def modify(self, algorithm):
-        self.update_variables(algorithm.iteration)
-
-
-class ThresholdSupport(ModifyAlgorithm):
-    """Update the support by thresholding a blurred version of real-space"""
-    def __init__(self, threshold, blur):
-        super().__init__()
-        self.add_variable(threshold, "threshold")
-        self.add_variable(blur, "blur")
-
-    def update_support(self, algorithm):
-        """Apply the support update rule for the real space in algorithm"""
-        iteration = algorithm.iteration
-        blurred_model = ndimage.gaussian_filter(abs(algorithm.real_model),
-                                                self.blur(iteration))
-        rescaled_threshold = blurred_model.max() * self.threshold(iteration)
-        algorithm.support = blurred_model > rescaled_threshold
-
-
-class AreaSupport(ModifyAlgorithm):
-    """Update the support to the densest part of real-space after blurring"""
-    def __init__(self, area, blur):
-        super().__init__()
-        self.add_variable(area, "threshold")
-        self.add_variable(blur, "blur")
-
-    def update_support(self, algorithm):
-        """Apply the support update rule for the real space in algorithm"""
-        iteration = algorithm.iteration
-        blurred_model = ndimage.gaussian_filter(abs(algorithm.real_model),
-                                                self.blur(iteration))
-        area_percent = self.area(iteration)/numpy.product(blurred_model.shape)
-        threshold = numpy.percentile(blurred_model.flat, area_percent)
-        algorithm.support = blurred_model > blurred_model.max() * threshold
-
-
-class CenterSupport(ModifyAlgorithm):
-    """Shift the support to the center of the field of view"""
-    def __init__(self, array_shape, kernel_sigma):
-        super().__init__()
-        arrays_1d = [numpy.arange(this_size) - this_size/2.
-                     for this_size in array_shape]
-
-        arrays_nd = numpy.meshgrid(*arrays_1d, indexing="ij")
-        radius2 = numpy.zeros(array_shape)
-        for this_dim in arrays_nd:
-            radius2 = radius2 + this_dim**2
-        gaussian_kernel = numpy.exp(-radius2/(2.*kernel_sigma**2))
-        self._kernel = fft.ifftshift(complex_type(gaussian_kernel))
-        self._kernel_ft_conj = numpy.conj(fft.fftn(self._kernel))
-        self._shape = array_shape
-
-    def _find_center(self, array):
-        conv = fft.ifftn(fft.fftn(fft.fftshift(array))*self._kernel_ft_conj)
-        pos = numpy.unravel_index(conv.argmax(), conv.shape)
-        return pos
-
-    def update_support(self, algorithm):
-        """Apply the support update rule for the real space in algorithm"""
-        pos = self._find_center(algorithm.support)
-        if backend == Backend.CUPY:
-            shift = [-p.get() for p in pos]
-        else:
-            shift = [-p for p in pos]
-        algorithm.support = numpy.roll(algorithm.support, shift=tuple(shift),
-                                       axis=tuple(range(len(shift))))
-
-
 class ConvexOptimizationAlgorithm:
     """Base class for all convex optimization algorithms"""
     def __init__(self, support=None, intensities=None,
                  amplitudes=None, mask=None,
                  real_model=None, fourier_model=None,
-                 constraints=None, link=None):
+                 constraints=None, link: ConvexOptimizationAlgorithm=None,
+                 copy: ConvexOptimizationAlgorithm=None):
 
         self._real_model = None
         self._support = None
 
-        if link is None:
+        if link is None and copy is None:
             if support is None:
                 raise ValueError("Must specify a support")
             self.support = support
@@ -207,9 +131,8 @@ class ConvexOptimizationAlgorithm:
                 self.fourier_model = fourier_model
             else:
                 # If no model is specified initialize with random phases
-                shape = self.amplitudes.shape
-                phases = 2*numpy.pi*numpy.random.random(shape)
-                self.fourier_model = self.amplitudes*numpy.exp(1.j*phases)
+                self.fourier_model = numpy.zeros(self.amplitudes.shape, dtype=complex_type)
+                self.set_random_phases()
 
             self._iteration = [0]
 
@@ -220,10 +143,12 @@ class ConvexOptimizationAlgorithm:
                     self.constraints = list(constraints)
                 except TypeError:
                     self.constraints = [constraints]
-        else:
+        elif link is not None:
             self.link(link)
+        else:
+            self.copy(copy)
 
-    def link(self, alg):
+    def link(self, alg: ConvexOptimizationAlgorithm) -> None:
         """Make this algorithm share all variables with another algorithm"""
         if not isinstance(alg, ConvexOptimizationAlgorithm):
             raise ValueError("link must be a ConvexOptimizationAlgorithm "
@@ -235,7 +160,15 @@ class ConvexOptimizationAlgorithm:
         self._iteration = alg._iteration
         self.constraints = alg.constraints
 
-    def is_linked(self, alg):
+    def copy(self, alg: ConvexOptimizationAlgorithm) -> None:
+        self._support = alg._support.copy()
+        self._amplitudes = alg._amplitudes.copy()
+        self._mask = alg._mask.copy()
+        self._real_model = alg._real_model.copy()
+        self._iteration = alg._iteration
+        self.constraints = [c for c in alg.constraints]
+
+    def is_linked(self, alg: ConvexOptimizationAlgorithm) -> bool:
         """Check if two algorithms are properly linked"""
         if (
                 id(self._support) == id(alg._support) and
@@ -248,11 +181,11 @@ class ConvexOptimizationAlgorithm:
             return False
 
     @property
-    def iteration(self):
+    def iteration(self) -> int:
         return self._iteration[0]
 
     @iteration.setter
-    def iteration(self, iteration):
+    def iteration(self, iteration: int):
         self._iteration[0] = iteration
 
     @property
@@ -269,7 +202,7 @@ class ConvexOptimizationAlgorithm:
         else:
             self._real_model = complex_type(fft.fftshift(real_model))
 
-    def link_model(self, alg):
+    def link_model(self, alg: ConvexOptimizationAlgorithm):
         """make the real_model ahared between two algorithms"""
         self._real_model = alg._real_model
 
@@ -278,13 +211,13 @@ class ConvexOptimizationAlgorithm:
         return fft.ifftshift(self._support)
 
     @support.setter
-    def support(self, support):
+    def support(self, support) -> None:
         if self._support is not None and self._support.shape == support.shape:
             self._support[...] = real_type(fft.fftshift(support))
         else:
             self._support = real_type(fft.fftshift(support))
 
-    def link_support(self, alg):
+    def link_support(self, alg: ConvexOptimizationAlgorithm) -> None:
         """make the support ahared between two algorithms"""
         self._support = alg._support
 
@@ -306,8 +239,8 @@ class ConvexOptimizationAlgorithm:
     def fourier_model_projected(self):
         fourier_model = fft.fftn(self._real_model)
         phases = numpy.angle(fourier_model)
-        projected = (self._amplitudes*numpy.exp(1.j*phases) +
-                     (1-self._mask)*fourier_model)
+        projected = (self._amplitudes*numpy.exp(1.j*phases)
+                     + (1-self._mask)*fourier_model)
         return fft.ifftshift(projected)
     # return fft.ifftshift(numpy.exp(1.j*phases)*self._amplitudes +
     #                      (1-self._mask)*fft.fftn(self._real_model))
@@ -327,21 +260,26 @@ class ConvexOptimizationAlgorithm:
     def mask(self):
         return fft.ifftshift(self._mask)
 
+    def set_random_phases(self) -> None:
+        shape = self.amplitudes.shape
+        phases = 2 * numpy.pi * numpy.random.random(shape)
+        self.fourier_model[...] = self.amplitudes * numpy.exp(1.j * phases)
+
     def fourier_space_constraint(self, data):
         """Apply the Fourier-space constraint to a real-space model"""
         data_ft = fft.fftn(data)
         phases = numpy.angle(data_ft)
         # result = fft.ifftn(self._mask*data_ft/abs(data_ft)*self._amplitudes +
         #                          (1-self._mask)*data_ft)
-        result = fft.ifftn(self._mask*self._amplitudes*numpy.exp(1.j*phases) +
-                           (1-self._mask)*data_ft)
+        result = fft.ifftn(self._mask*self._amplitudes*numpy.exp(1.j*phases)
+                           + (1-self._mask)*data_ft)
         return result
 
     def real_space_constraint(self, data):
         """Apply the real-space constraint to a real-space model"""
         return data*self._support
 
-    def fourier_space_error(self):
+    def fourier_space_error(self) -> float:
         """Fourier-space error of the current iterate"""
         fourier_model = fft.fftn(self._real_model)
         diff = abs(fourier_model*self._mask) - self._amplitudes
@@ -351,7 +289,7 @@ class ConvexOptimizationAlgorithm:
         fourier_error = numpy.sqrt(((diff / rescaling)**2).sum())
         return float(fourier_error)
 
-    def real_space_error(self):
+    def real_space_error(self) -> float:
         """Real-space error of the current iterate"""
         before_projection = self.fourier_space_constraint(self._real_model)
         diff = abs(before_projection*(1-self._support))
@@ -362,14 +300,15 @@ class ConvexOptimizationAlgorithm:
     def real_model_projected(self):
         return fft.ifftshift(self._real_model*self._support)
 
-    def iterate(self):
+    def iterate(self, niterations: int=1) -> None:
         """Progress the algorithm one iteration. Call this function!"""
-        self._iteration[0] += 1
-        self.update()
-        for constraint in self.constraints:
-            constraint(self)
+        for _ in range(niterations):
+            self._iteration[0] += 1
+            self.update()
+            for constraint in self.constraints:
+                constraint(self)
 
-    def update(self):
+    def update(self) -> None:
         """Update the iterate. Should be overloaded in subclasses. Will be
         called by iterate() so don't call it yourself.
 
@@ -393,21 +332,21 @@ class RelaxedAveragedAlternatingReflectors(ConvexOptimizationAlgorithm):
     def update(self):
         # Rs*Rm+I = (2*Ps-I)*(2*Pm-I)+I = 4*Ps*Pm - 2*Ps - 2*Pm + 2*I
         model_fc = self.fourier_space_constraint(self._real_model)
-        self._real_model[:] = (0.5*self.beta *
-                               (2.*self._real_model -
-                                2.*model_fc -
-                                2.*self._real_model*self._support +
-                                4.*model_fc*self._support) +
-                               (1-self.beta)*model_fc)
+        self._real_model[:] = (0.5*self.beta
+                               * (2.*self._real_modeln
+                                  - 2.*model_fc
+                                  - 2.*self._real_model*self._support
+                                  + 4.*model_fc*self._support)
+                               + (1-self.beta)*model_fc)
 
 
 class HybridInputOutput(ConvexOptimizationAlgorithm):
     """Hybrid input output (HIO) algorithm"""
-    def __init__(self, beta, *args, **kwargs):
+    def __init__(self, beta: float, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.beta = beta
 
-    def update(self):
+    def update(self) -> None:
         model_fc = self.fourier_space_constraint(self._real_model)
         self._real_model[:] = (self._support*model_fc +
                                (1-self._support)*(self._real_model -
@@ -416,24 +355,24 @@ class HybridInputOutput(ConvexOptimizationAlgorithm):
 
 class DifferenceMap(ConvexOptimizationAlgorithm):
     """Difference map algorithm"""
-    def __init__(self, beta, gamma_s=None, gamma_m=None, *args, **kwargs):
+    def __init__(self, beta: float, gamma_s=None, gamma_m=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.beta = beta
         sigma = 0.1
         if gamma_s is None:
             # self.gamma_s = - 1/self.beta
-            self.gamma_s = - ((4 + (2+beta)*sigma + beta*sigma**2) /
-                              (beta*(4 - sigma + sigma**2)))
+            self.gamma_s = - ((4 + (2+beta)*sigma + beta*sigma**2)
+                              / (beta*(4 - sigma + sigma**2)))
         else:
             self.gamma_s = gamma_s
         if gamma_m is None:
             # self.gamma_m = 1/self.beta
-            self.gamma_m = ((6 - 2*sigma - beta*(2 - 3*sigma + sigma**2)) /
-                            (beta*(4 - sigma + sigma**2)))
+            self.gamma_m = ((6 - 2*sigma - beta*(2 - 3*sigma + sigma**2))
+                            / (beta*(4 - sigma + sigma**2)))
         else:
             self.gamma_m = gamma_m
 
-    def update(self):
+    def update(self) -> None:
         model_fc = self.fourier_space_constraint(self._real_model)
         model_rc = self.real_space_constraint(self._real_model)
 
@@ -444,13 +383,108 @@ class DifferenceMap(ConvexOptimizationAlgorithm):
                                             self.fourier_space_constraint(fs)))
 
 
-def reality_constraint(algorithm):
+class ModifyAlgorithm:
+    """Abstract class for algorithms that work on phasing algorithms"""
+    def __init__(self):
+        pass
+
+    def add_variable(self, variable: str, name: str):
+        if not callable(variable):
+            # Hack to get the current 'variable' back, otherwise it
+            # will capture the overwritten function itself
+            def variable(iteration, v=variable):
+                return v
+            # variable = lambda iteration, v=variable: v
+        self.__dict__[name] = variable
+
+    def modify(self, algorithm: ConvexOptimizationAlgorithm) -> None:
+        self.update_variables(algorithm.iteration)
+
+
+class ThresholdSupport(ModifyAlgorithm):
+    """Update the support by thresholding a blurred version of real-space"""
+    def __init__(self, threshold: float, blur: float):
+        super().__init__()
+        self.add_variable(threshold, "threshold")
+        self.add_variable(blur, "blur")
+
+    def update_support(self, algorithm: ConvexOptimizationAlgorithm) -> None:
+        """Apply the support update rule for the real space in algorithm"""
+        iteration = algorithm.iteration
+        blurred_model = ndimage.gaussian_filter(abs(algorithm.real_model),
+                                                self.blur(iteration))
+        rescaled_threshold = blurred_model.max() * self.threshold(iteration)
+        algorithm.support = blurred_model > rescaled_threshold
+
+
+class AreaSupport(ModifyAlgorithm):
+    """Update the support to the densest part of real-space after blurring"""
+    def __init__(self, area: float, blur: float):
+        super().__init__()
+        self.add_variable(area, "threshold")
+        self.add_variable(blur, "blur")
+
+    def update_support(self, algorithm: ConvexOptimizationAlgorithm) -> None:
+        """Apply the support update rule for the real space in algorithm"""
+        iteration = algorithm.iteration
+        blurred_model = ndimage.gaussian_filter(abs(algorithm.real_model),
+                                                self.blur(iteration))
+        area_percent = self.area(iteration)/numpy.product(blurred_model.shape)
+        threshold = numpy.percentile(blurred_model.flat, area_percent)
+        algorithm.support = blurred_model > blurred_model.max() * threshold
+
+
+class CenterSupport(ModifyAlgorithm):
+    """Shift the support to the center of the field of view"""
+    def __init__(self, array_shape: Iterable[int], kernel_sigma: float):
+        super().__init__()
+        arrays_1d = [numpy.arange(this_size) - this_size/2.
+                     for this_size in array_shape]
+
+        arrays_nd = numpy.meshgrid(*arrays_1d, indexing="ij")
+        radius2 = numpy.zeros(array_shape)
+        for this_dim in arrays_nd:
+            radius2 = radius2 + this_dim**2
+        gaussian_kernel = numpy.exp(-radius2/(2.*kernel_sigma**2))
+        self._kernel = fft.ifftshift(complex_type(gaussian_kernel))
+        self._kernel_ft_conj = numpy.conj(fft.fftn(self._kernel))
+        self._shape = array_shape
+
+    def _find_center(self, array) -> tuple:
+        conv = fft.ifftn(fft.fftn(fft.fftshift(array))*self._kernel_ft_conj)
+        pos = numpy.unravel_index(conv.argmax(), conv.shape)
+        return pos
+
+    def update_support(self, algorithm: ConvexOptimizationAlgorithm) -> None:
+        """Apply the support update rule for the real space in algorithm"""
+        pos = self._find_center(algorithm.support)
+        if backend == Backend.CUPY:
+            shift = [-p.get() for p in pos]
+        else:
+            shift = [-p for p in pos]
+        algorithm.support = numpy.roll(algorithm.support, shift=tuple(shift),
+                                       axis=tuple(range(len(shift))))
+
+
+def reality_constraint(algorithm: ConvexOptimizationAlgorithm) -> None:
     algorithm._real_model[...] = numpy.real(algorithm._real_model)
 
 
-def positivity_constraint(algorithm):
+def positivity_constraint(algorithm: ConvexOptimizationAlgorithm) -> None:
     real_part = numpy.real(algorithm._real_model)
     real_part[real_part < 0.] = 0
+
+def refine_and_update_support(phasing_alg: ConvexOptimizationAlgorithm,
+                              support_alg: typing.Union[ModifyAlgorithm,
+                              collections.abc.Iterable[ModifyAlgorithm]],
+                              nrefine_iterations: int=5):
+    if isinstance(support_alg, ModifyAlgorithm):
+        support_alg = [support_alg]
+    refine_alg = ErrorReduction(copy=phasing_alg)
+    refine_alg.iterate(nrefine_iterations)
+    for this_support_alg in support_alg:
+        this_support_alg.update_support(refine_alg)
+    phasing_alg.support = refine_alg.support
 
 
 class CombineReconstructions:
